4ed0d7add0768b30c4f5c945555af8a5512b8d8d
tajain-mdsol <tajain@mdsol.com>
2023-05-15 20:23:35 +0530

    adds slice and returns map

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -7,19 +7,78 @@
 // 	return message
 // }
 
+// -----------------------------------------
+
+// package greetings
+
+// import (
+// 	"errors"
+// 	"fmt"
+// )
+
+// func Hello(name string) (string, error) {
+// 	// If no name is given return a n error.
+// 	if name == "" {
+// 		return "", errors.New("Empty Name!")
+// 	}
+
+// 	message := fmt.Sprintf("Hi, %v. Welcome", name)
+// 	return message, nil
+// }
+
+// -----------------------------------------
+
 package greetings
 
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
-	// If no name is given return a n error.
+	// If no name was given, return an error with a message.
 	if name == "" {
-		return "", errors.New("Empty Name!")
+		return name, errors.New("Empty Name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := [] string {
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	// Return a randomly selected message format by specifying a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
+}
+
+// -------------------------------------------------------------------------------
+// Hellos returns a map that associates each of the named people with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+
+	// Loop through the received slice of names, calling the Hello function to get a message for each name
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// In the map, associate the retrieved message with the name.
+		messages[name] = message
+	}
+
+	return messages, nil
 }
--- hello_/hello_.go
+++ hello_/hello_.go
@@ -9,10 +9,16 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("")
+	// message, err := greetings.Hello("")
 	// message, err := greetings.Hello("Tanmay")
+
+	// A slice of names.
+	names := [] string {"Kane", "Son", "Kulu"}
+	messages, err := greetings.Hellos(names)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	// fmt.Println(message)
+	fmt.Println(messages)
 }
