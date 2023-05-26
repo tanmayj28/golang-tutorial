7e2bcec06ff4687d9fa7149cd5749beb30e8af5d
tajain-mdsol <tajain@mdsol.com>
2023-05-26 14:40:45 +0530

    adds userinput main.go

---
+++ 03userinput/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main() {
+	welcome := "Welcome to userimput program"
+	fmt.Println(welcome)
+
+	// reader is listening and reading here
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating of Tottenham:")
+
+	// comma ok || err ok
+	// use _ for outputs you don't care about
+
+	// read till someone hits \n
+	// input, err := reader.ReadString('\n')
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating, ", input)
+	fmt.Printf("Type of rating is %T", input)
+}
